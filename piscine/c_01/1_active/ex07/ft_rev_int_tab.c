32b827b602a4d575b5286fa649c6becd5b53ce75
Juan Diosdado <jdiosdad@student.42.fr>
2022-08-25 18:28:57 +0200

    ex07

--- piscine/c_01/1_active/ex07/ft_rev_int_tab.c
+++ piscine/c_01/1_active/ex07/ft_rev_int_tab.c
@@ -3,61 +3,71 @@
 /*                                                        :::      ::::::::   */
 /*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xuanmir <xuanmir@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jdiosdad <jdiosdad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 16:02:51 by jdiosdad          #+#    #+#             */
-/*   Updated: 2022/08/25 15:50:47 by xuanmir          ###   ########.fr       */
+/*   Updated: 2022/08/25 18:26:32 by jdiosdad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putstr(char *str);
+// void	ft_putstr(char *str);
 
-void	ft_swap(int *a, int *b);
+// void	ft_swap(int *a, int *b);
 
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	reverse[size];
 	int	index;
-	// int	*reverse;
-
-	// index = 0;
-	// while (tab[index] != '\0')
-	// {
-	// 	size--;
-	// 	reverse[size] = tab[index];
-	// 	index++;
-	// }
-	// *tab = *reverse;
-
-	// index = 0;
-	// while (tab[index])
-	// {
-	// 	if (tab[index])
-	// 	{
-	// 		reverse[size - 1] = tab[index];
-	// 		size--;
-	// 	}
-	// 	index++;
-	// }
-	// reverse[size - 1] = tab[index];
-	// *tab = *reverse;
 
 	index = 0;
-	while (reverse[index] != '\0')
+	while (tab[index] != '\0')
 	{
-		reverse[index] = tab[size - 1];
-		index++;
 		size--;
+		reverse[size] = tab[index];
+		index++;
 	}
 	*tab = *reverse;
-	// ft_swap(tab, reverse);
 }
 
-void	ft_swap(int *a, int *b)
-{
-	int	temp;
+// void	ft_rev_int_tab(int *tab, int size)
+// {
+// 	int	reverse[size];
+// 	int	index;
 
-	temp = *b;
-	*b = *a;
-	*a = temp;
-}
+// 	index = 0;
+// 	while (tab[index])
+// 	{
+// 		if (tab[index])
+// 		{
+// 			reverse[size - 1] = tab[index];
+// 			size--;
+// 		}
+// 		index++;
+// 	}
+// 	reverse[size - 1] = tab[index];
+// 	*tab = *reverse;
+// }
+
+// void	ft_rev_int_tab(int *tab, int size)
+// {
+// 	int	reverse[size];
+// 	int	index;
+
+// 	index = 0;
+// 	while (reverse[index] != '\0')
+// 	{
+// 		reverse[index] = tab[size - 1];
+// 		index++;
+// 		size--;
+// 	}
+// 	*tab = *reverse;
+// }
+
+// void	ft_swap(int *a, int *b)
+// {
+// 	int	temp;
+
+// 	temp = *b;
+// 	*b = *a;
+// 	*a = temp;
+// }
--- piscine/c_01/1_active/ex07/main.c
+++ piscine/c_01/1_active/ex07/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xuanmir <xuanmir@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jdiosdad <jdiosdad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 16:03:00 by jdiosdad          #+#    #+#             */
-/*   Updated: 2022/08/25 15:47:55 by xuanmir          ###   ########.fr       */
+/*   Updated: 2022/08/25 18:20:28 by jdiosdad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_rev_int_tab(int *tab, int size);
 
 int	main(void)
 {
-	int tab[] = {1, 2, 3};
+	int	tab[] = {1, 2, 3};
 
 	printf("Original: %i, %i, %i\n", tab[0], tab[1], tab[2]);
 	ft_rev_int_tab(tab, 3);
