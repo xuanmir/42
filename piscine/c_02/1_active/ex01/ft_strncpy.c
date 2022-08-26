053b82ef87224ab16d4ae0aa9d30ea7d32933f6e
Juan Diosdado <jdiosdad@student.42.fr>
2022-08-26 23:00:26 +0200

    Update ex01

--- piscine/c_02/1_active/ex01/ft_strncpy.c
+++ piscine/c_02/1_active/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: jdiosdad <jdiosdad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 19:06:34 by jdiosdad          #+#    #+#             */
-/*   Updated: 2022/08/25 19:08:26 by jdiosdad         ###   ########.fr       */
+/*   Updated: 2022/08/26 22:59:51 by jdiosdad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,23 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 
 	index = 0;
 }
+
+/*
+check man strncpy
+*/
+
+/*
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	index;
+
+	index = 0;
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
+*/
--- piscine/c_02/1_active/ex01/main.c
+++ piscine/c_02/1_active/ex01/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xuanmir <xuanmir@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jdiosdad <jdiosdad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 19:07:22 by jdiosdad          #+#    #+#             */
-/*   Updated: 2022/08/26 02:33:50 by xuanmir          ###   ########.fr       */
+/*   Updated: 2022/08/26 22:59:16 by jdiosdad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n);
 
 int	main(void)
 {
+	char			dest[1337];
+	char			src[] = "Coucou Pauline, merci pour le bulletproof d'amour <3";
+	unsigned int	n;
+
+	ft_strncpy(dest, src, n);
+	printf("%s\n", dest);
 	return (0);
 }
